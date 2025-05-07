9ce83f80c07d00c0cc0cc1739a6734496e1a871b
Francesco Penasa <francesco.penasa.job@gmail.com>
2025-05-07 15:36:35 +0200

    add movement to snake

--- snake/main.lua
+++ snake/main.lua
@@ -23,6 +23,7 @@ function love.load()
     love.window.setTitle("Snake Game") -- Set the window title
 
     math.randomseed(os.time()) -- Seed the random number generator
+    lastUpdate = 0 -- Variable to track the last update time
 
     -- load fonts
     smallFont = love.graphics.newFont('retro.ttf', 8)
@@ -46,7 +47,41 @@ function love.load()
 end
 
 function love.update(dt)
+    -- limit the frame rate to 60 FPS
+    if love.timer.getTime() - lastUpdate > 1 / 10 then -- Check if the time since the last update is greater than 1/60 seconds
+        lastUpdate = love.timer.getTime() -- Update the last update time
+    else
+        return -- If not, return to limit the frame rate
+    end
+
     -- Update game logic
+    -- handle input 
+    if love.keyboard.isDown('up') and direction ~= 'down' then -- If the up key is pressed and the snake is not moving down
+        direction = 'up' -- Change direction to up
+    elseif love.keyboard.isDown('down') and direction ~= 'up' then -- If the down key is pressed and the snake is not moving up
+        direction = 'down' -- Change direction to down
+    elseif love.keyboard.isDown('left') and direction ~= 'right' then -- If the left key is pressed and the snake is not moving right
+        direction = 'left' -- Change direction to left
+    elseif love.keyboard.isDown('right') and direction ~= 'left' then -- If the right key is pressed and the snake is not moving left
+        direction = 'right' -- Change direction to right
+    end
+
+    -- move the body
+    for i = #snake, 2, -1 do -- Iterate through the snake segments from the end to the beginning
+        snake[i].x = snake[i - 1].x -- Set the x position of the current segment to the x position of the previous segment
+        snake[i].y = snake[i - 1].y -- Set the y position of the current segment to the y position of the previous segment
+    end
+
+    -- move the head
+    if direction == 'up' then -- If the direction is up
+        snake[1].y = snake[1].y - SNAKE_SIZE -- Move the head up
+    elseif direction == 'down' then -- If the direction is down
+        snake[1].y = snake[1].y + SNAKE_SIZE -- Move the head down
+    elseif direction == 'left' then -- If the direction is left
+        snake[1].x = snake[1].x - SNAKE_SIZE -- Move the head left
+    elseif direction == 'right' then -- If the direction is right
+        snake[1].x = snake[1].x + SNAKE_SIZE -- Move the head right
+    end
 end
 
 function love.draw()
